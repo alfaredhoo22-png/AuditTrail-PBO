cc5474e5ee70f9b30651de1355a3d5960fbd116a
alfaredhoo22-png <alfaredhoo22@gmail.com>
2026-07-10 00:32:28 +0700

    Update DatabaseConfig.java

--- src/main/java/config/DatabaseConfig.java
+++ src/main/java/config/DatabaseConfig.java
@@ -10,7 +10,7 @@ public class DatabaseConfig {
     // Konfigurasi Database PostgreSQL melalui Supabase (Shared Pooler)
     private static final String URL = "jdbc:postgresql://aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres";
     private static final String USER = "postgres.grmewrzgjwjiauiimxuz";
-    private static final String PASSWORD = "aldo22@";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
